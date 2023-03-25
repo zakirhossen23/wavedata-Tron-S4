98bcc30561437c07b3e2d8b7fe4cec4d4007aa66
zakirhossen23 <73053667+zakirhossen23@users.noreply.github.com>
2023-03-26 03:18:17 +0600

    fixed cors

--- wavedata-api/contract/fixCors.js
+++ wavedata-api/contract/fixCors.js
@@ -5,9 +5,10 @@ async function Headers(res){
         // another common pattern
         // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
         res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader('x-Trigger', 'CORS')
         res.setHeader(
           'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type,Origin, Date, X-Api-Version,Authorization, X-API-KEY, Cache-Control, Pragma,Access-Control-Allow-Request-Method'
         )
     } catch (error) {
         console.log(error)
